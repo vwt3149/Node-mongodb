e09b6d536e14ad95332ca88700aa3f750289f9e1
Adrijan <adrijna149@gmail.com>
2019-02-20 10:06:21 +0100

    add get todos rout

--- server/server.js
+++ server/server.js
@@ -8,36 +8,47 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());//middleware 3th party
-// To create a resourse
+
+
+// To create a resourse, POST
 app.post('/todos', (req,res) => {
     var todo = new Todo({
         text:req.body.text,
-        completed:req.body.completed, 
-        completedAt:req.body.completedAt
+        // completed:req.body.completed, 
+        // completedAt:req.body.completedAt
     });
 
     todo.save().then((result) => {
         // sending back to user information
-        res.send(200,result)
+        res.send(result)
+        console.log();
+        console.log(`Successful\n ${result}`);
 
 
-        // console.log('Successful',result);
     }).catch((err) => {
         // if error occur, user will get message
-        res.send(400,err);
-
-
-        // console.log('Error', err);
+        res.status(400).send(err);
+        console.log();
+        // console.log(`--Error occured-- ${err}`);
     });
-})
+});// End POST
 
+
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((result) => {
+        res.send({result});
+    }, (err) => {
+        res.status(400).send(err);
+    })
+});// End GET
 
 app.listen(8080, () => {
     console.log();
     console.log('Server started on port 8080');
 });
 
-
+module.exports = {app}
 
 
 
